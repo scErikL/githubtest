c8302c00fe0962ac6a99fff0383c060c4531a65e
Erik Lövbom <erik.lovbom@studentconsulting.com>
2016-08-17 08:34:12 +0200

    hej

---
+++ githubtest4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="githubtest4.MvcApplication" Language="C#" %>
---
+++ githubtest4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(githubtest4.Startup))]
+namespace githubtest4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
